c9d4616f3946b345e500c0c8607a76d60290d774
Mohammed Arbaz <mohammed.arbaz@zensar.com>
2022-05-20 10:40:59 +0530

    BlackJack

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            var s = new shuffle();
+    
+            var dealer1 = s.Shuffle();
+            var dealer2 = s.Shuffle();
+            var player1 = s.Shuffle();
+            var player2 = s.Shuffle();
+
+            ViewBag.Dealer = string.Format("{0}, {1} ", dealer1, dealer2);
+            ViewBag.Player = string.Format("{0}, {1} ", player1, player2);
+
+            int intdealer1 = s.convertToInt(dealer1);
+            int intdealer2 = s.convertToInt(dealer2);
+            int intPlayer1 = s.convertToInt(player1);
+            int intPlayer2 = s.convertToInt(player2);
+
+            int SumofDealer = intdealer1 + intdealer2;
+            ViewBag.SumDealer = string.Format("{0}", SumofDealer);
+            int SumofPlayer = intPlayer1 + intPlayer2;
+            ViewBag.SumPlayer = string.Format("{0}", SumofPlayer);
+            
+            return View();
+        }
+
+       
+
+        [HttpGet]
+        public ActionResult HitButton()
+        {
+            shuffle Hit = new shuffle();
+            TempData["Hit"] = "";
+            //TempData["Sum"] = "";
+            return View(Hit);
+        }
+        
+        [HttpPost]
+        //public ActionResult HitButton(shuffle sf)
+        //{
+        //    int t1 = sf.HitMethod();
+        //    TempData["Hit"] =  string.Format("{0}", t1);
+
+        //    int sum = t1;
+        //    TempData["Sum"] = string.Format("{0}", sum);
+
+        //    return RedirectToAction("Index");
+        //}
+
+
+        public ActionResult HitButton(shuffle sf)
+        {
+
+            TempData["Hit"] = TempData["Details"] + string.Format("{0}", sf.HitMethod());
+
+            return RedirectToAction("Index");
+        }
+
+        //[HttpPost]
+        //public ActionResult StayButton(shuffle ss)
+        //{
+        //    TempData["Stay"] = TempData["Stay"].ToString() + "," + string.Format("{0}", ss.HitMethod());
+        //    return RedirectToAction("Index");
+        //}
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/shuffle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class shuffle
+    {
+        public string Shuffle()
+        {
+            List<string> list1 = new List<string>()
+            {
+                "Ace", "2" ,"3","4","5","6","7","8","9","10","Jack","Queen","King"
+            };
+
+            Random rnd = new Random();
+
+            int index = rnd.Next(list1.Count);
+
+            return list1[index];
+            
+
+        }
+
+        public int convertToInt(string s)
+        {
+                int returnValue;
+                switch (s)
+                {
+                    case "Ace":
+                        returnValue = 11;
+                        break;
+                    case "King":
+                    case "Queen":
+                    case "Jack":
+                    case "10":
+                        returnValue = 10;
+                        break;
+                    case "9":
+                        returnValue = 9;
+                        break;
+                    case "8":
+                        returnValue = 8;
+                        break;
+                    case "7":
+                        returnValue = 7;
+                        break;
+                    case "6":
+                        returnValue = 6;
+                        break;
+                    case "5":
+                        returnValue = 5;
+                        break;
+                    case "4":
+                        returnValue = 4;
+                        break;
+                    case "3":
+                        returnValue = 3;
+                        break;
+                    case "2":
+                        returnValue = 2;
+                        break;
+                    default:
+                        returnValue = 0;
+                        break;
+                }
+                return returnValue;
+        }
+
+        public int HitMethod()
+        {
+            Random r = new Random();
+            int Cardvalue = r.Next(1,11);
+            return Cardvalue;
+        }
+   
+
+
+    }
+}
